8b8e3b7952d59676855213d99ff04108deda6318
zhang <GZHANG58@volvocars.com>
2024-11-26 03:04:19 +0100

    update code

--- push-code.bat
+++ push-code.bat
@@ -1,20 +1,19 @@
 @echo off
-REM 变量定义
 set REPO_URL=https://github.com/KexVol/prefect-process-worker.git
 set BRANCH=main
-set COMMIT_MESSAGE=更新代码
+set COMMIT_MESSAGE=update code
 
-REM 初始化 Git 仓库（如果尚未初始化）
+REM Ini if not
 if not exist ".git" (
     git init
     git remote add origin %REPO_URL%
 )
 
-REM 添加所有更改到暂存区
+REM added to temp
 git add .
 
-REM 提交更改
+REM submit commit
 git commit -m "%COMMIT_MESSAGE%"
 
-REM 推送更改到远程仓库
+REM push to
 git push -u origin %BRANCH%
